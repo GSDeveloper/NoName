e8af48572c87b1fd132b16b70eed818502d71305
GS PLUGINS II Developer <51029268+ImmortalityDeveloper@users.noreply.github.com>
2021-03-26 16:48:12 +0300

    Add files via upload

---
+++ CoreConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Rocket.API;
+
+namespace GS.NoName
+{
+	public class CoreConfig : IRocketPluginConfiguration, IDefaultable
+	{
+		public void LoadDefaults()
+		{
+			characterName = "Неизвестный";
+		}
+
+		public string characterName;
+	}
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Permissions;
+using SDG.Unturned;
+using Steamworks;
+
+namespace GS.NoName
+{
+	public class Plugin : RocketPlugin<CoreConfig>
+	{
+        protected override void Load()
+		{
+			Plugin.Instance = this;
+            UnturnedPermissions.OnJoinRequested -= new UnturnedPermissions.JoinRequested(this.OnJoinRequested);
+		}
+
+		public void OnJoinRequested(CSteamID PlayerID, ref ESteamRejection? RejectionReason)
+		{
+			SteamPending steamPending = Provider.pending.FirstOrDefault((SteamPending SP) => SP.playerID.steamID == PlayerID);
+			steamPending.playerID.characterName = base.Configuration.Instance.characterName;
+		}
+
+		protected override void Unload()
+		{
+			UnturnedPermissions.OnJoinRequested -= new UnturnedPermissions.JoinRequested(this.OnJoinRequested);
+		}
+
+		public static Plugin Instance;
+	}
+}
